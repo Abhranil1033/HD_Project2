235fec7b34143a8f7d6b04602c4a0707af22c575
dishita10 <dishuhazarika@gmail.com>
2023-06-26 23:38:06 +0530

    changes

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className='container'>
-            <div className='top-half my-5'>
-                <h1 className='top'>Searching for ?</h1>
+        <div className='home-container'>
+            <div className='home-top-half my-5'>
+                <h1 className='home-top'>Searching for ?</h1>
             </div>
-            <div className='bottom-half'>
-                <div className='flats'>
-                    <Link className='flatsbutton'to="/flats">FLATS</Link>
+            <div className='home-bottom-half'>
+                <div className='home-flats'>
+                    <Link className='home-flatsbutton'to="/flats">FLATS</Link>
                 </div>
-                <div className='flatmates'>
-                    <Link className='flatmatesbutton'to="/flatmates">FLATMATES</Link>
+                <div className='home-flatmates'>
+                    <Link className='home-flatmatesbutton'to="/flatmates">FLATMATES</Link>
                 </div>
             </div>
         </div>
