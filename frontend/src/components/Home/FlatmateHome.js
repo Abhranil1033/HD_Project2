75d4fa0bc33ec40d6a91c9e9c4bc0ba13598274a
dishita10 <dishuhazarika@gmail.com>
2023-07-01 23:21:01 +0530

    one card component centered

--- frontend/src/components/Home/FlatmateHome.js
+++ frontend/src/components/Home/FlatmateHome.js
@@ -1,33 +1,60 @@
 import React from 'react';
 import "./FlatmateHome.css"
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 const FlatmateHome = () => {
   const [cards]=useState([
     {
       title:'Person-1',
-      image:"./user&flat/user.png"
+      image:"./user&flat/user.png",
+      age:20,
+      gender:"Female",
+      occupation:"Software Engineer"
     },
     {
       title:'Person-2',
-      image:"./user&flat/user.png"
+      image:"./user&flat/user.png",
+      age:20,
+      gender:"Female",
+      occupation:"Software Engineer"
     },
     {
       title:'Person-3',
-      image:"./user&flat/user.png"
+      image:"./user&flat/user.png",
+      age:20,
+      gender:"Female",
+      occupation:"Software Engineer"
     },
     {
       title:'Person-4',
-      image:"./user&flat/user.png"
+      image:"./user&flat/user.png",
+      age:20,
+      gender:"Female",
+      occupation:"Software Engineer"
     },
     {
       title:'Person-5',
-      image:"./user&flat/user.png"
+      image:"./user&flat/user.png",
+      age:20,
+      gender:"Female",
+      occupation:"Software Engineer"
     },
     {
       title:'Person-6',
-      image:"./user&flat/user.png"
+      image:"./user&flat/user.png",
+      age:20,
+      gender:"Female",
+      occupation:"Software Engineer"
     },
   ])
+  useEffect(() => {
+    const cardContainer = document.querySelector('.cards');
+    if (cards.length === 1) {
+      cardContainer.classList.add('centered');
+    } else {
+      cardContainer.classList.remove('centered');
+    }
+  }, [cards.length]);
+
   return (
     <div className='flatmatePage'>
       <section>
@@ -36,9 +63,14 @@ const FlatmateHome = () => {
             {
               cards.map((cards,i)=>(
                   <div key={i} className="card">
-                    <h3>{cards.title}</h3>
                     <img src={cards.image} alt="images"/>
-                    <button className="flatmate-button">View</button>
+                    <h3>{cards.title}</h3>
+                    <div className="special">
+                    <h6>{cards.occupation}</h6>|
+                    <h6 className="h63">{cards.gender}</h6>|
+                    <h6 className="h62 ">{cards.age}</h6>
+                    </div>
+                    <button className="flatmate-button">Find out more</button>
                   </div> 
               ))
             }
