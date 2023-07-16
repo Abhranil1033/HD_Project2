0ccd5bd696703ad70f895627f517857a6feb72ff
Abhranil <deyabhranil1033@gmail.com>
2023-07-16 14:39:37 +0530

    fixed navbar

--- frontend/src/components/layout/Navbar/Navbar.js
+++ frontend/src/components/layout/Navbar/Navbar.js
@@ -6,41 +6,41 @@ import UserOptions from "../../User/UserOptions.js";
 import Loader from "../Loader/Loader.js"
 
 const Navbar = () => {
-    const { loading, isAuthenticated, user } = useSelector((state) => state.user);
-    return (
-        <div className="navbar" style={{ color: "white" }}>
-            {loading ? <Loader /> : (
-                <nav className="navbar navbar-expand-lg bg-body-dark fixed-top">
-                    <Link className="navbar-brand" to="/">FlatHunt</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" style={{ marginRight: '10 px' }}></span>
-                    </button>
+  const { loading, isAuthenticated, user } = useSelector((state) => state.user);
+  return (
+    <div className="navbar" style={{ color: "white" }}>
+      {loading ? <Loader /> : (
+        <nav className="navbar navbar-expand-lg bg-body-dark fixed-top">
+          <Link className="navbar-brand navbar-brand-margin" to="/">FlatHunt</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" style={{ marginRight: '10 px' }}></span>
+          </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/search">Search</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/cart">Cart</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="#">List Property</Link>
-                            </li>
-                        </ul>
-                        
-                        <div className="d-flex align-items-center"> {/* Ensures buttons are aligned properly */}
-                            {!isAuthenticated && <Link to="/login" className="btn btn-secondary mx-2">Sign Up</Link>}
-                            {isAuthenticated && <UserOptions user={user} />}
-                        </div>
-                    </div>
-                </nav>
-            )}
-        </div>
-    )
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/search">Search</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">Cart</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">List Property</Link>
+              </li>
+            </ul>
+
+            <div className="d-flex align-items-center"> {/* Ensures buttons are aligned properly */}
+              {!isAuthenticated && <Link to="/login" className="btn btn-secondary mx-2">Sign Up</Link>}
+              {isAuthenticated && <UserOptions user={user} />}
+            </div>
+          </div>
+        </nav>
+      )}
+    </div>
+  )
 }
 
 export default Navbar;
